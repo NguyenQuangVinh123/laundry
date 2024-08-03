58205ae67bbd1c50d743f29fb219471d4d3693b5
vinhnguyen <88311053+vinhnq-devblock@users.noreply.github.com>
2024-08-03 17:08:28 +0700

    add light

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { usePathname, useRouter } from "next/navigation";
 import Link from "next/link";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -39,6 +40,7 @@ export default function RootLayout({
         </ul>
 
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
