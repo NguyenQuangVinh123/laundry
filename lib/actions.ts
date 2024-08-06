a58b4c8767138ac079a5997a1fc5f244457b1a28
vinhnguyen <88311053+vinhnq-devblock@users.noreply.github.com>
2024-08-06 11:20:03 +0700

    refactor

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,47 +4,37 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export const saveContact = async (prevSate: any, formData: FormData) => {
-  const customerIdInput = Number(formData.get("customerId"));
-  const amount = Number(formData.get("amount"));
+  const getListCustomer = await prisma.customer.findMany()
+  const listNameCustomer = getListCustomer.map(i => i.id)
+  let customerId = Number(formData.get("customerId"))
+  if(!listNameCustomer.includes(customerId)) {
+    const res = await prisma.customer.create({
+      data: {
+        name: formData.get("customerId") as string
+      }
+    })
+    customerId = res.id
+  }
 
   try {
-    // Start a transaction
-    const result = await prisma.$transaction(async (prisma) => {
-      let customerId = customerIdInput;
-      let customer = await prisma.customer.findUnique({
-        where: { id: customerId || 0 },
-      });
-
-      // If customer doesn't exist, create a new one
-      if (!customer) {
-        customer = await prisma.customer.create({
-          data: { name: formData.get("customerId") as string },
-        });
-        customerId = customer.id;
-      }
-
-      // Create a new bill and update the customer record
-      await prisma.bill.create({
-        data: {
-          customerId: customerId,
-          amount,
-        },
-      });
-
-      await prisma.customer.update({
-        where: { id: customerId },
-        data: {
-          totalUsed: { increment: 1 },
-          dateUsed: { push: new Date() },
-        },
-      });
-
-      return customerId;
+    await prisma.bill.create({
+      data: {
+        customerId: customerId,
+        amount: Number(formData.get("amount"))
+      },
     });
-    // Redirect and revalidate
-   
+    await prisma.customer.update({
+      where: { id: customerId },
+      data: {
+        totalUsed: {
+          increment: 1
+        },
+        dateUsed: {
+          push : new Date()
+        }
+      }
+    })
   } catch (error) {
-    console.error("Error saving contact:", error);
     return { message: "Failed to create contact" };
   }
   revalidatePath("/contacts");
