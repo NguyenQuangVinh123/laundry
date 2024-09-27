413a87365b4970a803342b8a3319471abe1aba63
vinhnguyen <88311053+vinhnq-devblock@users.noreply.github.com>
2024-09-27 23:15:40 +0700

    update

--- components/search.tsx
+++ components/search.tsx
@@ -5,12 +5,11 @@ import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 
 const Search = () => {
-  const searchParams = useSearchParams();
+  const searchParams: any = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term: string) => {
-    // console.log(term);
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set("query", term);
